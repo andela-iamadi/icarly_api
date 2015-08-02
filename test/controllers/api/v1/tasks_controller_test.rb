f76a0536b3e18f70b9627272be0ba4e833d7b119
Innocent Amadi <innocent.amadi@andela.com>
2015-08-02 17:46:16 +0100

    Updated task controller

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -15,27 +15,6 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
     assert_equal 3, tasks["message"].count
   end
 
-
-  test "should get delete" do
-    # require 'pry-nav'; binding.pry
-    # get :delete
-    # assert_response :success
-  end
-
-  test "the index action returns back json of all test" do
-    task1 = Task.create(task: "Amity rooftop party", alias: "party", due_time: "12:00", user_id: 1)
-    task2 = Task.create(task: "Thoughtspot cleansing", alias: "party", due_time: "12:00", user_id: 1)
-    task3 = Task.create(task: "Checkout race", alias: "party", due_time: "12:00", user_id: 1)
-
-    get :index
-    tasks = JSON.parse(response.body)
-    require 'pry'; binding.pry
-    assert_equal 3, tasks.count
-    assert_equal "Amity rooftop party", tasks[0]
-    assert_equal "Thoughtspot cleansing", tasks[1]
-    assert_equal "Checkout race", tasks[2]
-  end
-
   test "the index action returns task list arranged with most recent at the top" do
     get :index, id: @user.id, user: { username: "elis" }
     tasks = JSON.parse(response.body)
@@ -81,26 +60,6 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
     assert_no_difference '@user.tasks.count' do
       delete :destroy, id: "", task: { due_date: Date.current() }, user: { username: @user.username }
     end
-
-  test "should get delete" do
-    # require 'pry-nav'; binding.pry
-    # get :delete
-    # assert_response :success
-  end
-
-  test "the index action returns back json of all test" do
-    task1 = Task.create(task: "Amity rooftop party", alias: "party", due_time: "12:00", user_id: 1)
-    task2 = Task.create(task: "Thoughtspot cleansing", alias: "party", due_time: "12:00", user_id: 1)
-    task3 = Task.create(task: "Checkout race", alias: "party", due_time: "12:00", user_id: 1)
-
-    get :index
-    tasks = JSON.parse(response.body)
-    require 'pry'; binding.pry
-    assert_equal 3, tasks.count
-    assert_equal "Amity rooftop party", tasks[0]
-    assert_equal "Thoughtspot cleansing", tasks[1]
-    assert_equal "Checkout race", tasks[2]
-
   end
 
 end
